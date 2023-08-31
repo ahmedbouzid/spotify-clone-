c668677851b48d8878f04fc5fa3fce6405e433dd
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-31 10:22:57 +0200

    create home page view - fetch data from fake collection

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  songsCards = [
+     {
+        "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+        "title": "Titre de la Chanson 1",
+        "description": "Description de la Chanson 1"
+    },
+    {
+        "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+        "title": "Titre de la Chanson 2",
+        "description": "Description de la Chanson 2"
+    },
+    {
+      "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+      "title": "Titre de la Chanson 2",
+      "description": "Description de la Chanson 2"
+  },
+  {
+    "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+    "title": "Titre de la Chanson 2",
+    "description": "Description de la Chanson 2"
+},
+{
+  "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+  "title": "Titre de la Chanson 2",
+  "description": "Description de la Chanson 2"
+},
+{
+  "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+  "title": "Titre de la Chanson 2",
+  "description": "Description de la Chanson 2"
+},
+{
+  "playlistThumbnail": "https://ik.imagekit.io/azn6jixxj/song.jfif?updatedAt=1693418220627",
+  "title": "Titre de la Chanson 2",
+  "description": "Description de la Chanson 2"
+},
+
+
+]
+
+
 }
